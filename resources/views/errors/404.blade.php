6c3f12c6a0c084a8ee00ecd718d38e9abb0d56ed
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-14 05:37:25 +0330

    create 404.blade.php

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>ادمین تو - قالب مدیریتی واکنش گرا</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="یک قالب مدیریتی با امکانات فراوان برای ساخت سی آر ام، سیستم مدیریت محتوا و ..." name="description" />
+        <meta content="قائم امیدی" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/app-rtl.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="authentication-bg">
+
+        <div class="account-pages mt-5 mb-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="text-center">
+                           
+                        </div>
+                        <div class="card">
+
+                            <div class="card-body p-4">
+
+                                <div class="text-center">
+                                    <h1 class="text-error">404</h1>
+                                    <h3 class="mt-3 mb-2">برگه پیدا نشد</h3>
+                                    <p class="text-muted mb-3">متاسفیم! چیزی که به دنبال آن می گردید، یافت نشد.</p>
+
+                                    <a href="index.html" class="btn btn-danger waves-effect waves-light"><i class="fas fa-home mr-1"></i> بازگشت به خانه</a>
+                                </div>
+
+
+                            </div> <!-- end card-body -->
+                        </div>
+                        <!-- end card -->
+
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end page -->
+
+        <!-- Vendor js -->
+        <script src="assets/js/vendor.min.js"></script>
+
+        <!-- App js-->
+        <script src="assets/js/app.min.js"></script>
+
+    </body>
+</html>

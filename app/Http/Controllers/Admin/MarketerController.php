7ced4e0795f7fe442e60d071cc4788b520f2b428
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-17 12:44:42 +0330

    implement index() in Admin\MarketerController

--- app/Http/Controllers/Admin/MarketerController.php
+++ app/Http/Controllers/Admin/MarketerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Marketer;
+use DataTables;
 
 class MarketerController extends Controller
 {
@@ -12,9 +14,38 @@ class MarketerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if ($request->ajax()) {
+            $marketers = Marketer::with('user','commission');
+            return Datatables::of($marketers)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Edit" class="edit btn btn-icon waves-effect waves-light btn-warning editUser"><i class="fa fa-edit"></i></a>';
+                    $btn = $btn . ' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Delete" class="btn btn-icon waves-effect waves-light btn-danger deleteUser"><i class="fas fa-trash"></i></a>';
+                    return $btn;
+                })->addColumn('name', function (Marketer $marketer) {
+                    return $marketer->user->name;
+
+                })->addColumn('last_name', function (Marketer $marketer) {
+                    return $marketer->user->last_name;
+
+
+                })->addColumn('email', function (Marketer $marketer) {
+                    return $marketer->user->email;
+
+
+                })->addColumn('email', function (Marketer $marketer) {
+                    return $marketer->user->email;
+
+
+                })->addColumn('phone', function (Marketer $marketer) {
+                    return $marketer->user->phone;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+        return view('admin.marketer.index');
     }
 
     /**

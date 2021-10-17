9189b9cf68eea00cd9fec0b253dd4bfdc48477ae
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-17 12:19:04 +0330

    create relationship between Marketer and User model

--- app/Marketer.php
+++ app/Marketer.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Marketer extends Model
 {
     protected $fillable = ['user_id','tel','address','national_code','status','parent_id'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
 }

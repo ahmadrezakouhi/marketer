b2f4254ad7faee033d3791a73476d32b8a028c95
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-20 11:56:38 +0330

    create login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,170 +1,76 @@
-{{-- @extends('layouts.main') --}}
 
-{{-- @section('content') --}}
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+@extends('layouts.main')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+@section('content')
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<div class="home-btn d-none d-sm-block">
+    <a href="index.html"><i class="fas fa-home h2 text-dark"></i></a>
+</div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<div class="account-pages mt-5 mb-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6 col-xl-5">
+                <div class="text-center">
+                    <a href="index.html">
+                        <span><img src="assets/images/logo-dark.png" alt="تصویر" height="22"></span>
+                    </a>
+                    <p class="text-muted mt-2 mb-4">قالب مدیریتی واکنش گرا</p>
                 </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
+                <div class="card">
 
-{{-- @endsection --}}
+                    <div class="card-body p-4">
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>ورود</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="یک قالب مدیریتی با امکانات فراوان برای ساخت سی آر ام، سیستم مدیریت محتوا و ..." name="description" />
-        <meta content="قائم امیدی" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
-
-        <!-- App css -->
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/app-rtl.min.css" rel="stylesheet" type="text/css" />
-
-    </head>
-
-    <body class="authentication-bg">
-
-        <div class="home-btn d-none d-sm-block">
-            <a href="index.html"><i class="fas fa-home h2 text-dark"></i></a>
-        </div>
-
-        <div class="account-pages mt-5 mb-5">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-                        <div class="text-center">
-
+                        <div class="text-center mb-4">
+                            <h4 class="text-uppercase mt-0">ورود</h4>
                         </div>
-                        <div class="card">
 
-                            <div class="card-body p-4">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form-group mb-3">
+                                <label for="emailaddress">ایمیل</label>
+                                <input class="form-control" type="email" id="email" name="email" value="{{ old('email') }}" autocomplete="email" autofocus placeholder="ایمیل خود را وارد کنید">
+                            </div>
 
-                                <div class="text-center mb-4">
-                                    <h4 class="text-uppercase mt-0">ورود</h4>
+                            <div class="form-group mb-3">
+                                <label for="password">رمز عبور</label>
+                                <input class="form-control" type="password"  id="password" name="password" required autocomplete="current-password" placeholder="رمز عبور خود را وارد کنید">
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="custom-control-label" for="remember">من را به خاطر بسپار</label>
                                 </div>
+                            </div>
 
-                                <form method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <div class="form-group mb-3">
-                                        <label for="emailaddress">ایمیل</label>
-                                        <input class="form-control " type="email" id="email" name="email" placeholder="ایمیل خود را وارد کنید">
-                                    </div>
+                            <div class="form-group mb-0 text-center">
+                                <button class="btn btn-primary btn-block" type="submit"> ورود </button>
+                            </div>
 
-                                    <div class="form-group mb-3">
-                                        <label for="password">رمز عبور</label>
-                                        <input class="form-control" type="password"  id="password" name="password" placeholder="رمز عبور خود را وارد کنید">
-                                    </div>
+                        </form>
 
-                                    <div class="form-group mb-3">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                            <label class="custom-control-label" for="remember">من را به خاطر بسپار</label>
-                                        </div>
-                                    </div>
+                    </div> <!-- end card-body -->
+                </div>
+                <!-- end card -->
 
-                                    <div class="form-group mb-0 text-center">
-                                        <button class="btn btn-primary btn-block" type="submit"> ورود </button>
-                                    </div>
-                                    <div class="row mt-3">
-                                        <div class="col-12 text-center">
-
-                                            <p> <a href="{{route('password.request')}}" class="text-muted ml-1"><i class="fa fa-lock mr-1"></i>رمز عبور خود را فراموش کرده اید؟</a></p>
-
-
-                                        </div> <!-- end col -->
-                                    </div>
-                                </form>
-
-                            </div> <!-- end card-body -->
-                        </div>
-                        <!-- end card -->
-
-
-                        <!-- end row -->
-
+                <div class="row mt-3">
+                    <div class="col-12 text-center">
+                        <p> <a href="pages-recoverpw.html" class="text-muted ml-1"><i class="fa fa-lock mr-1"></i>رمز عبور خود را فراموش کرده اید؟</a></p>
+                        <p class="text-muted">حساب کاربری ندارید؟ <a href="pages-register.html" class="text-dark ml-1"><b>ثبت نام کنید</b></a></p>
                     </div> <!-- end col -->
                 </div>
                 <!-- end row -->
-            </div>
-            <!-- end container -->
+
+            </div> <!-- end col -->
         </div>
-        <!-- end page -->
+        <!-- end row -->
+    </div>
+    <!-- end container -->
+</div>
 
-        <!-- Vendor js -->
-        <script src="assets/js/vendor.min.js"></script>
 
-        <!-- App js-->
-        <script src="assets/js/app.min.js"></script>
+@endsection
 
-    </body>
-</html>

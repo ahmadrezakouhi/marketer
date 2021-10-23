0e8d652275a43eaeaf62c5d0a8df86ed1e5e13a8
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-23 10:50:10 +0330

    implement index() of Marketer/MarketerController

--- app/Http/Controllers/Marketer/MarketerController.php
+++ app/Http/Controllers/Marketer/MarketerController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Marketer;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\User;
+use App\Marketer;
+use DataTables;
 class MarketerController extends Controller
 {
     /**
@@ -12,9 +14,37 @@ class MarketerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if ($request->ajax()) {
+            $marketers = Marketer::find(1)->marketers()->with('user');
+            return Datatables::of($marketers)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Edit" class="edit btn btn-icon waves-effect waves-light btn-warning editMarketer"><i class="fa fa-edit"></i></a>';
+                    $btn = $btn . ' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Delete" class="btn btn-icon waves-effect waves-light btn-danger deleteMarketer"><i class="fas fa-trash"></i></a>';
+                    return $btn;
+                })->addColumn('name', function (Marketer $marketer) {
+                    return $marketer->user->name;
+                })->addColumn('last_name', function (Marketer $marketer) {
+                    return $marketer->user->last_name;
+                })->addColumn('email', function (Marketer $marketer) {
+                    return $marketer->user->email;
+                })->addColumn('email', function (Marketer $marketer) {
+                    return $marketer->user->email;
+                })->addColumn('phone', function (Marketer $marketer) {
+                    return $marketer->user->phone;
+                })
+                ->editColumn('status', function (Marketer $marketer) {
+                    if ($marketer->status) {
+                        return 'فعال';
+                    }
+                    return 'غیر فعال';
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+        return view('marketer.marketers.index');
     }
 
     /**

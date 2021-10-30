ccf8f030b940b54a6e9661baa3f929b09434807b
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-30 09:05:41 +0330

    remove relationship between User and Wallet model

--- app/Wallet.php
+++ app/Wallet.php
@@ -7,8 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Wallet extends Model
 {
     protected $fillable = ['user_id','wallet'];
-    public function user()
-    {
-        return $this->belongsTo('App\User');
-    }
+    
 }

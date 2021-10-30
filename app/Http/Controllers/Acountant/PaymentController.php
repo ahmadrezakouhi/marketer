549d26f5e3616ac99d800fbcdf9c38b526b52b0f
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-30 20:38:54 +0330

    implement index() in Acountant/PaymentController

--- app/Http/Controllers/Acountant/PaymentController.php
+++ app/Http/Controllers/Acountant/PaymentController.php
@@ -4,8 +4,45 @@ namespace App\Http\Controllers\Acountant;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Payment;
+use DataTables;
 
 class PaymentController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $payments = Payment::with('card');
+            return Datatables::of($payments)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Edit" class="edit btn btn-icon waves-effect waves-light btn-success acceptCard"><i class="fas fa-check"></i></a>';
+                    $btn = $btn . ' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Delete" class="btn btn-icon waves-effect waves-light btn-danger declineCard"><i class="fas fa-ban"></i></a>';
+                    return $btn;
+                })
+                ->addColumn('fullname', function (Payment $payment) {
+
+                    return $payment->card->marketer->user->fullname;
+                })
+                ->addColumn('identification', function (Payment $payment) {
+
+                    return $payment->card->identification;
+                })
+                ->editColumn('status', function (Payment $payment) {
+                    if ($payment->status == -1) {
+                        return 'رد شده';
+                    } else if ($payment->status == 0) {
+                        return 'در حال بررسی';
+                    } else {
+                        return 'تایید شده';
+                    }
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+
+
+        return view('acountant.payments.index');
+    }
 }

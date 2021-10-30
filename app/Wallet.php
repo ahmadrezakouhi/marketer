d0f76898131c6af075595e6ef796e0d6767d74af
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-10-30 09:09:59 +0330

    create relationship between Marketer and Wallet

--- app/Wallet.php
+++ app/Wallet.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Wallet extends Model
 {
     protected $fillable = ['user_id','wallet'];
-    
+
+    public function marketer()
+    {
+        return $this->belongsTo('App\Marketer');
+    }
 }
